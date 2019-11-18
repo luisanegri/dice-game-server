7eab3b840ad13e1644bef48d35fad833ff3fadc6
shuaibcs <alshuaibi7@gmail.com>
2019-11-18 11:44:33 +0100

    get request test

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+
+
+app.get('/', (re, res, next) => {
+  res.send('Hola')
+})
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => console.log(`listeninig at Port ${port}`));
+
