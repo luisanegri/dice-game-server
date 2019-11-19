d5a6d6b3abd822b01c14569144cbdbd905ce3434
luisanegri <luisaanegri@gmail.com>
2019-11-19 13:18:06 +0100

    find all rooms created and send through the stream

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-const Sequelize = require('sequelize')
-const databaseURL = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+const Sequelize = require('sequelize');
+const databaseURL =
+  process.env.DATABASE_URL ||
+  'postgres://postgres:6772@localhost:5432/postgres';
 
-const db = new Sequelize(databaseURL)
+const db = new Sequelize(databaseURL);
 
-db 
-    .sync()
-    .then(() => console.log('Database has been updated'))
-    .catch(console.error)   
+db.sync()
+  .then(() => console.log('Database has been updated'))
+  .catch(console.error);
 
-
-module.exports = db 
+module.exports = db;
--- room/router.js
+++ room/router.js
@@ -1,24 +1,20 @@
-const { Router } = require('express')
-const Room = require('./model')
+const { Router } = require('express');
+const Room = require('./model');
 
-function roomFactory(stream){
-  const router = new Router()
-///Create Room route
-router.post(
-  '/room',
-  (request, response, next) => {
-    Room
-    .create(request.body)
-    .then(room => {
-      const data = JSON.stringify(room)
-      stream.send(data)
-      response.send(room)
-    })
-  }
-)
-return router
+function roomFactory(stream) {
+  const router = new Router();
+  ///Create Room route
+  router.post('/room', (request, response, next) => {
+    Room.create(request.body).then(() => {
+      Room.findAll().then(rooms => {
+        console.log({ allRooms: rooms.map(r => r.dataValues) });
+        stream.send(rooms);
+      });
+      //console.log({ roomData: room });
+      //response.send(room);
+    });
+  });
+  return router;
 }
 
-
-
-module.exports = roomFactory
+module.exports = roomFactory;
